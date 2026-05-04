20f6298a09661c4961f0d7fa04ce969ab9910df2
Yan Toton <develroman320@gmail.com>
2026-05-05 02:36:54 +0600

    Add web_developer class with details method

---
+++ program @_+ 84.( class with details method).py
@@ -0,0 +1,11 @@
+class web_developer:
+    def __init__(self,role,language):
+        self.role = role
+        self.language = language
+
+
+    def show_details(self):
+        return f"Hi it's Yan. I am a {self.role} and I use the {self.language} programming language for web design work.."
+
+aboutObj = web_developer("web_developer","Python")
+print(aboutObj.show_details())
