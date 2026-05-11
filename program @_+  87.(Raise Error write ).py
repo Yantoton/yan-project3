b48130875ad645070b2db38defc0fac44c94a534
Yan Toton <develroman320@gmail.com>
2026-05-11 20:30:50 +0600

    Implement error handling and character printing
    Add error handling for empty name variable and print each character.
    
    খালি 'name' ভেরিয়েবলের জন্য ত্রুটি ব্যবস্থাপনা যোগ করুন এবং প্রতিটি অক্ষর প্রিন্ট করুন।

---
+++ program @_+  87.(Raise Error write ).py
@@ -0,0 +1,9 @@
+name = "Mahiyan"
+if name == "":
+    raise ValueError("The name variables is empty:")
+for names in name:
+    print(names)
+    if names == "M":
+     print("Capital Letter")
+else:
+    print(f"Hello {name} The Lord Of Darkness")
